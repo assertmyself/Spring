9054650fa3f8260f5a21e35b2b4f571b85901a6f
syz <sunyanzheng@gbcom.com.cn>
2016-08-19 13:21:19 +0800

    code update; author : syz comment: modify the code ,devlep status

--- mvc/src/main/java/com/springapp/mvc/HelloController.java
+++ mvc/src/main/java/com/springapp/mvc/HelloController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.System;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+        System.out.println("hello");
 		return "hello";
 	}
 
