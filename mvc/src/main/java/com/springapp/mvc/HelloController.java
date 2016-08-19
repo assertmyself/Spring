aae847cfc039bc264595ddccb5cc35e22819433a
syz <sunyanzheng@gbcom.com.cn>
2016-08-19 13:16:26 +0800

    First commit

---
+++ mvc/src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,24 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+
+    @RequestMapping("/do")
+    public String doNext(ModelMap model,String name,String age){
+        model.addAttribute("message","name is :"+name+"  ;; the age is :"+age) ;
+        System.out.println(name+";"+age);
+
+        return "hello";
+    }
+}
